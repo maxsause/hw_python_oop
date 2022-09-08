25e2a83595861ae03ee148d9688ac2dc5457565e
Максим Белов <maxandmax337@gamil.com>
2022-09-08 13:09:57 +0300

    first review 1.3

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ class InfoMessage:
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type};'
-                + f' Длительность: {self.duration} ч.;'
+                + f' Длительность: {self.duration:.3f} ч.;'
                 + f' Дистанция: {self.distance:.3f} км;'
                 + f' Ср. скорость: {self.speed:.3f} км/ч;'
                 + f' Потрачено ккал: {self.calories:.3f}.')
